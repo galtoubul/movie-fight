06b4fb1b8727df7558823b492d9b9bdfa75d6124
galtoubul <galtoubul@gmail.com>
2022-05-13 18:08:48 +0300

    added a debounce api call to omdbapi

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("Hi there!");
+const fetchData = async (searchTerm) => {
+  const response = await axios.get("http://www.omdbapi.com/", {
+    params: { apikey: "a39d71c7", s: searchTerm },
+  });
+
+  if (response.data.Error) {
+    return [];
+  }
+  return response.data.Search;
+};
+
+const input = document.querySelector("input");
+
+const onInput = async (event) => {
+  const movies = await fetchData(event.target.value);
+
+  for (let movie of movies) {
+    const div = document.createElement("div");
+    div.innerHTML = `
+    <img src="${movie.Poster}"/>
+    <h1>${movie.Title}</h1>
+    `;
+    document.querySelector("#target").appendChild(div);
+  }
+};
+
+input.addEventListener("input", debounce(onInput, 500));
