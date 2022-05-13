515edebeb9b0f7aa8714465ddc1d39535addf864
galtoubul <galtoubul@gmail.com>
2022-05-13 19:07:56 +0300

    fetching and showing selected movie details

--- index.js
+++ index.js
@@ -43,9 +43,10 @@ const onInput = async (event) => {
     <img src="${imgSrc}"/>
     ${movie.Title}
     `;
-    option.addEventListener("click", (event) => {
+    option.addEventListener("click", () => {
       dropdown.classList.remove("is-active");
       input.value = movie.Title;
+      onMovieSelect(movie);
     });
     resultsWrapper.appendChild(option);
   }
@@ -58,3 +59,30 @@ document.addEventListener("click", (event) => {
     dropdown.classList.remove("is-active");
   }
 });
+
+const onMovieSelect = async (movie) => {
+  const response = await axios.get("http://www.omdbapi.com/", {
+    params: { apikey: "a39d71c7", i: movie.imdbID },
+  });
+  console.log(response.data);
+  document.querySelector("#summary").innerHTML = movieTemplate(response.data);
+};
+
+const movieTemplate = (movieDetail) => {
+  return `
+    <article class="media>
+        <figure class="media-left">
+            <p class='img'>
+                <img src="${movieDetail.Poster}" />
+            </p>
+        </figure>
+        <div class="media-content">
+            <div class="content">
+                <h1>${movieDetail.Title}</h1>
+                <h4>${movieDetail.Genre}</h4>
+                <p>${movieDetail.Plot}</p>
+            </div>
+        </div>
+    </article>
+    `;
+};
